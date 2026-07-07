57345ec5c092cfa19f3dc911b9936f34e199f04f
victorsa72 <64659541+victorsa72@users.noreply.github.com>
2026-07-07 09:16:20 -0700

    Add supabase staging keys

--- webhost.sh
+++ webhost.sh
@@ -1,1 +1,5 @@
-flutter run -d web-server --web-port 8080 --web-hostname 0.0.0.0
+#!/bin/bash
+flutter run -d web-server --web-port 8080 --web-hostname 0.0.0.0 \
+  --dart-define=SUPABASE_ENV=staging \
+  --dart-define=SUPABASE_URL="https://kawtsuhiogeszsvgyyld.supabase.co" \
+  --dart-define=SUPABASE_PUB_KEY="sb_publishable_yBAZIbXqjquvOegsVG85tg_6SXCqxm4"
